0edec04dfa3c7cac1d568d9135b12d834eea2ec0
bruno-perspectives <bruno@perspective-s.org>
2022-01-13 14:12:52 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const fs = require('fs');
+
+const app = express()
+
+const PORT = 3000
+const FILE_PATH = "c:/temp"
+
+app.use (function(req, res, next) {
+  var data='';
+  req.setEncoding('utf8');
+  req.on('data', function(chunk) { 
+     data += chunk;
+  });
+  req.on('end', function() {
+      req.body = data;
+      next();
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+  
+  if(!fs.existsSync(FILE_PATH)) fs.mkdirSync(FILE_PATH)
+  fs.writeFileSync(`${FILE_PATH}/${Date.now().valueOf()}.xyz`, req.body)
+  res.send('ok')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
